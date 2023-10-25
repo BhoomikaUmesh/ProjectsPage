61e6429f2912129288f7907f36630e7378d6ad81
BhoomikaUmesh <bhoomikaumesh2105@gmail.com>
2023-10-25 14:27:48 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/ProjectsPage.js 	modified:   src/index.css 	new file:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ProjectsPage from './ProjectsPage';
 
 function App() {
+  const simulatedProjects = [
+    {
+      _id: 1,
+      project_image: 'https://via.placeholder.com/300',
+      project_name: 'Project 1',
+      project_description: 'Description for Project 1',
+    },
+    {
+      _id: 2,
+      project_image: 'url-to-image-2',
+      project_name: 'Project 2',
+      project_description: 'Description for Project 2',
+    },
+    {
+      _id: 3,
+      project_image: 'url-to-image-3',
+      project_name: 'Project 3',
+      project_description: 'Description for Project 3',
+    },
+    {
+      _id: 4,
+      project_image: 'url-to-image-4',
+      project_name: 'Project 4',
+      project_description: 'Description for Project 4',
+    },
+    {
+      _id: 5,
+      project_image: 'url-to-image-5',
+      project_name: 'Project 5',
+      project_description: 'Description for Project 5',
+    },
+    {
+      _id: 6,
+      project_image: 'url-to-image-6',
+      project_name: 'Project 6',
+      project_description: 'Description for Project 6',
+    },
+    {
+      _id: 7,
+      project_image: 'url-to-image-7',
+      project_name: 'Project 7',
+      project_description: 'Description for Project 7',
+    },
+    {
+      _id: 8,
+      project_image: 'url-to-image-8',
+      project_name: 'Project 8',
+      project_description: 'Description for Project 8',
+    },
+    {
+      _id: 9,
+      project_image: 'url-to-image-9',
+      project_name: 'Project 9',
+      project_description: 'Description for Project 9',
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProjectsPage projects={simulatedProjects} />
     </div>
   );
 }
---
+++ src/ProjectsPage.js
@@ -0,0 +1,77 @@
+// ProjectsPage.jsx
+import React from 'react';
+
+const ProjectsPage = ({ projects }) => {
+  const handleDelete = (projectId) => {
+    console.log(`Deleting project with ID: ${projectId}`);
+  };
+
+  const handleUpdate = (projectId) => {
+    console.log(`Updating project with ID: ${projectId}`);
+  };
+
+  const handleUpload = () => {
+    console.log('Handling upload...');
+    // Implement your upload logic here
+  };
+
+  return (
+    <div className="container mx-auto mt-8 bg-gradient-to-b from-blue-100 to-blue-500 p-8">
+      <h1 className="text-3xl font-bold mb-4 text-center">PROJECTS</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-9 ">
+        {projects.map((project) => (
+          <div
+            key={project._id}
+            className="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-b from-cyan-500 to-blue-100 transform hover:scale-105 transition-transform"
+          >
+            <img
+              src={project.project_image || 'https://via.placeholder.com/300'}
+              alt={project.project_name}
+              className="w-full h-60 object-cover"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{project.project_name}</div>
+              <p className="text-gray-700 text-base">
+                {project.project_description}
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              {/* You can add your tags or additional information here */}
+            </div>
+            <div className="px-6 pt-4 pb-2 flex justify-between">
+              <button
+                onClick={() => handleUpdate(project._id)}
+                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 mr-2"
+              >
+                Update
+              </button>
+              <button
+                onClick={() => handleDelete(project._id)}
+                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-end mt-8">
+        <button className="bg-green-400 text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-900 hover:bg-green-900 hover:text-white shadow-md py-3 px-8 inline-flex items-center">
+          <svg
+            fill="#FFF"
+            height="18"
+            viewBox="0 0 24 24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
+          </svg>
+          <span className="ml-2">Upload</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsPage;
